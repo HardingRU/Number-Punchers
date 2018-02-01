3a6fe7d92e1708e5d129f473b9d8d5d24d6ad376
Chris Harding <christopher.harding@gmx.com>
2018-02-01 09:08:44 -0500

    proposal, basic JS logic

---
+++ app.js
@@ -0,0 +1,232 @@
+// need to create logic to prevent user from moving off of matrix
+
+// the initial game state, number represents the number you are finding multiples of.
+// number should be randomly set as part of board creation (fix this)
+// gameType is multiples (1), factors (2), and prime (3)
+
+// ****NOTE TO SELF do I need to ever pass the gameState to other functions, or can I just reference this one?
+let gameState = {
+  score: 0,
+  lives: 4,
+  number: 2,
+  gameType: 3,
+  xPos: 0,
+  yPos: 0
+}
+
+// this class defines a Gameboard, and was borrowed from eloquent javascript chap 5
+class Gameboard {
+  constructor(width, height, content = () => undefined) {
+    this.width = width;
+    this.height = height;
+    this.content = [];
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.content[y * width + x] = content(x, y);
+      }
+    }
+  }
+
+  get(x, y) {
+    return this.content[y * this.width + x];
+  }
+  set(x, y, value) {
+    this.content[y * this.width + x] = value;
+  }
+}
+let board = newGame(gameState);
+// this function creates a new gameboard
+function createBoard() {
+  let board = new Gameboard(6, 5);
+  for (let i = 0; i <= board.width; i++) {
+    for (let j = 0; j <= board.height; j++) {
+      board.set(i, j, randInt());
+    }
+  }
+  return board;
+}
+
+// write board to the dom
+function placeBoard() {
+  document.createElement('table');
+
+}
+
+// this function creates random integers, and is used to create the board
+function randInt() {
+  randDec = Math.random();
+  randWhole = Math.floor(randDec * 20) + 1;
+  return randWhole;
+}
+
+// this function is called when the user munchers a number, to see if it is correct or not
+function checkAnswer(state, inboard) {
+  let answer = board.get(state.xPos, state.yPos);
+  console.log(answer);
+  // checking for empty space, nothing should happen here
+  if (answer === null) {
+    return;
+  }
+
+  // logic to proceed on correct answer
+  switch(state.gameType) {
+    // check answer against multiple algorithm
+    case(1):
+    if (answer % state.number === 0) {
+      state.score += 50;
+      board.set(state.xPos, state.yPos, null);
+      let levelClear = checkBoard(state, inboard);
+      // if the game is over create a new level
+      if(levelClear === true) {
+        newGame(state);
+      }
+    }
+    // logic to proceed on wrong answer
+    else {
+      wrongAnswer();
+    }
+    break;
+
+    // check answer against factor algorithm
+    case(2):
+    if (state.number % answer === 0) {
+      state.score += 50;
+      board.set(state.xPos, state.yPos, null);
+      let levelClear = checkBoard(state, inboard);
+      // if the game is over create a new level
+      if(levelClear === true) {
+        newGame(state);
+      }
+    }
+    else {
+      wrongAnswer();
+    }
+    break;
+
+    // check answer against prime algorithm
+    case(3):
+    if(isPrime(answer) === true) {
+      state.score += 50;
+      board.set(state.xPos, state.yPos, null);
+      let levelClear = checkBoard(state, inboard);
+      // if the game is over create a new level
+      if(levelClear === true) {
+        newGame(state);
+      }
+    }
+    else {
+      wrongAnswer();
+    }
+    break;
+  }
+
+  gameState = state;
+  board = inboard;
+  return;
+}
+
+function wrongAnswer() {
+  gameState.lives -= 1;
+  if(gameState.lives === 0) {
+    console.log("Sorry, game over!");
+    // ****** create a fuction here to deal with game over (do this)
+    return;
+  }
+}
+// this function is called when user munchers correct number, to see if they have beaten the level
+function checkBoard(state, inboard) {
+  for(let i = 0; i <= board.width; i++) {
+    for(let j = 0; j <= board.height; j++) {
+      switch(state.gameType) {
+        // checking board for any remaining multiples
+        case(1):
+        if(board.get(state.xPos,state.yPos) % state.number === 0) {
+          return false;
+        }
+        break;
+
+        // checking board for any remaining factors
+        case(2):
+        if(state.number % (board.get(state.xPos,state.yPos)) === 0) {
+          return false;
+        }
+        break;
+
+        // checking board for any remaining primes
+        case(3):
+        if(isPrime(board.get(state.xPos, state.yPos)) === false){
+          return false;
+        }
+        break;
+      }
+    }
+  }
+  return true;
+}
+
+// this runs to create a new game board on level completion
+function newGame(state) {
+  gameState = state;
+  gameState.xPos = 0;
+  gameState.yPos = 0;
+  let board = createBoard();
+  return board;
+}
+
+// handle movement and manipulation of xPos and yPos of gameState
+function move(direction) {
+  switch(direction) {
+    case("u"):
+      if(gameState.yPos != 0) {
+      gameState.yPos -= 1;
+      }
+      break;
+    case("d"):
+      if(gameState.yPos != 4) {
+      gameState.yPos += 1;
+      }
+      break;
+    case("l"):
+      if(gameState.xPos != 0) {
+      gameState.xPos -= 1;
+      }
+      break;
+    case("r"):
+      if(gameState.xPos != 5) {
+      gameState.xPos += 1;
+      }
+      break;
+  }
+}
+
+function isPrime(value) {
+  for (let i = 2; i < value; i++) {
+    if (value % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function rightAnswer() {
+
+}
+
+console.log(board);
+console.log(gameState);
+move("l");
+checkAnswer(gameState, board);
+console.log(gameState);
+move("r");
+checkAnswer(gameState, board);
+console.log(gameState);
+move("r");
+checkAnswer(gameState, board);
+console.log(gameState);
+move("d");
+checkAnswer(gameState, board);
+console.log(gameState);
+move("d");
+checkAnswer(gameState, board);
+console.log(gameState);
---
+++ proposal.md
@@ -0,0 +1,24 @@
+Number Munchers Proposal
+
+*What is Number Munchers?*
+An eductional game for kids that teaches basic math skills, which I played a lot when I was younger.  The game is basically a 5x6 grid of numbers, and you are told to find and select all of the numbers that meet X criteria (i.e. all prime numbers, all multiples of 3, all factors of 15).  
+
+Finding all of the correct answers will bring you to the next level, and each correct answer will add to your score.  Selecting an incorrect answer will cause you to lose a life.  Players start with 4 lifes, and game is over once they hit 0.
+
+*Wireframe*
+(Your wireframes go here. Preferably two or more)
+
+*Initial thoughts on game structure*
+The real version of the game has 'enemies' that appear on screen and move around.  Running into the enemies loses a life.  This is probably not going to be in my initial scope, but I'd like to add it if time allows.
+
+The only other real difficult part I see is around the logic.  Assumuing that there will be three different types of game board (prime, factors, multiples), what numbers should I put on the board for each, and what critera will correspond for these board types.  Should the game get more challenging as you complete boards?  This would require even more logic.  
+
+*Phases of Completion*
+1) Create the logic in straightforward fashion
+2) Refactor Logic
+3) Create the visuals
+4) Add bells and whistles *optional*
+
+*Links and Resources*
+- Play the game here: https://classicreload.com/number-munchers.html
+- Borrowing the matrix example from Chapter 6 of Eloquent JS, as it is perfect for this game.
